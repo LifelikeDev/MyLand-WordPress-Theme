a2a97880cc2c0370bb6662e2dff781eadd844cfc
Navian <primeguyy@gmail.com>
2021-04-12 12:51:20 +0000

    second commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Main template file
+
+@package myland
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myland WordPress Theme</title>
+</head>
+
+<body>
+
+    <h1>Some pretty nice theing coming up dosoon.</h1>
+
+</body>
+
+</html>
